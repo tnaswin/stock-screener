f24950e3ee6c989612a7b33dfcc05ee0a63822db
aswin <tnaswin007@gmail.com>
2021-03-02 16:46:17 +0530

    Adding an async background task to update Stock data.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import models
-from fastapi import FastAPI, Request, Depends
+import yfinance
+from fastapi import FastAPI, Request, Depends, BackgroundTasks
 from fastapi.templating import Jinja2Templates
 from database import SessionLocal, engine
 from sqlalchemy.orm import Session
@@ -35,8 +36,31 @@ def home(request: Request):
     })
 
 
+def fetch_stock_data(id: int):
+    """
+
+    """
+    db = SessionLocal()
+
+    stock = db.query(Stock).filter(Stock.id == id).first()
+
+    yahoo_data = yfinance.Ticker(stock.symbol)
+
+    stock.ma200 = yahoo_data.info['twoHundredDayAverage']
+    stock.ma50 = yahoo_data.info['fiftyDayAverage']
+    stock.price = yahoo_data.info['previousClose']
+    stock.forward_pe = yahoo_data.info['forwardPE']
+    stock.forward_eps = yahoo_data.info['forwardEps']
+    stock.dividend_yield = yahoo_data.info['dividendYield'] * 100
+
+    db.add(stock)
+    db.commit()
+
+
 @app.post("/stock")
-def create_stock(stock_request: StockRequest, db: Session = Depends(get_db)):
+async def create_stock(stock_request: StockRequest,
+                       background_tasks: BackgroundTasks,
+                       db: Session = Depends(get_db)):
     """
     Create a stock and store in Database.
     """
@@ -44,6 +68,8 @@ def create_stock(stock_request: StockRequest, db: Session = Depends(get_db)):
     stock.symbol = stock_request.symbol
     db.add(stock)
     db.commit()
+
+    background_tasks.add_task(fetch_stock_data, stock.id)
 
     return {
         "code": "Success",
