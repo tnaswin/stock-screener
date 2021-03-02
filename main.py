d5e186c85aab5b072c3cfe0483817455c6db392a
aswin <tnaswin007@gmail.com>
2021-03-02 15:30:35 +0530

    Added a template using SemanticUI and Jinja2

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def home(request: Request):
+    """
+    Display the Homepage
+    """
+    return templates.TemplateResponse("home.html", {
+        'request': request
+    })
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/stock")
+def create_stock():
+    """
+    Create a stock and store in Database.
+    """
+    return {
+        "code": "Success",
+        "message": "Stock Created"
+    }
